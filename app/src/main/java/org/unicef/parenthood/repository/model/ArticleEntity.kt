b83c8d0038ec94f13e4b29c3fb518ce073de3548
kutovenko <kutovenko@gmail.com>
2019-11-22 20:33:10 +0300

    Entities updated

--- app/src/main/java/org/unicef/parenthood/repository/model/ArticleEntity.kt
+++ app/src/main/java/org/unicef/parenthood/repository/model/ArticleEntity.kt
@@ -2,12 +2,10 @@ package org.unicef.parenthood.repository.model
 
 import androidx.room.Entity
 //todo
-@Entity(tableName = "articles")
 data class ArticleEntity(
     val id: String,
     val title: String,
-    val tests: ArrayList<TestEntity>,
-    var reads: String,
-    val content: String, //что-то библиотечное из парсера рсс
-    var progress: Int //для прогрессбара
+    val test: TestEntity,
+    val content: String,
+    val categories: String
 )

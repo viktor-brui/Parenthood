1764bd431fc856ef79b8029fc76ba20f162f51b2
Veronika K <veronikakuzn99@gmail.com>
2019-11-22 20:50:17 +0300

    Sign in and sign up are created

---
+++ app/src/main/java/org/unicef/parenthood/ui/fragment/SignUpFragment.kt
@@ -0,0 +1,56 @@
+package org.unicef.parenthood.ui.fragment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import org.unicef.parenthood.ui.viewmodel.SignUpViewModel
+import org.unicef.parenthood.R
+
+class SignUpFragment : Fragment()  {
+    //todo
+    private lateinit var viewModel: SignUpViewModel
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "SignUpFragment"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    fun onNextClicked(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                }
+            }
+    }
+
+    companion object {
+        fun newInstance() = SignUpFragment()
+    }
+}
---
+++ app/src/main/java/org/unicef/parenthood/ui/viewmodel/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package org.unicef.parenthood.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel: ViewModel() {
+
+}

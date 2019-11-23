f333277240cd11c296dd0bd25126c49612b0bc48
artnest <artnest789@gmail.com>
2019-11-23 04:20:02 +0300

    Use Task-based coroutines API for Firebase

--- app/src/main/java/org/unicef/parenthood/ui/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/org/unicef/parenthood/ui/viewmodel/SignUpViewModel.kt
@@ -4,7 +4,10 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import org.unicef.parenthood.util.Event
 
 class SignUpViewModel : ViewModel() {
@@ -17,17 +20,17 @@ class SignUpViewModel : ViewModel() {
         get() = _isSuccessfulSignUp
 
     fun onRegisterClicked(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    _isSuccessfulSignUp.value = Event(true)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    _isSuccessfulSignUp.value = Event(false)
-                }
+        viewModelScope.launch {
+            try {
+                auth.createUserWithEmailAndPassword(email, password).await()
+
+                Log.d(TAG, "createUserWithEmail: success")
+
+                _isSuccessfulSignUp.value = Event(true)
+            } catch (e: Exception) {
+                Log.w(TAG, "createUserWithEmail: failure", e)
+                _isSuccessfulSignUp.value = Event(false)
             }
+        }
     }
 }

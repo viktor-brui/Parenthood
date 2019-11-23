443ea640b3f66b47976fd5d693f6e6e0bdc5f56d
Viktor <viktor-brui@mail.ru>
2019-11-23 03:22:43 +0300

    1

--- app/src/main/java/org/unicef/parenthood/ui/fragment/SignUpFragment.kt
+++ app/src/main/java/org/unicef/parenthood/ui/fragment/SignUpFragment.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
-import org.unicef.parenthood.R
 import org.unicef.parenthood.databinding.FragmentSignUpBinding
 import org.unicef.parenthood.ui.viewmodel.SignUpViewModel
-import org.unicef.parenthood.util.toast
 
 class SignUpFragment : Fragment()  {
     private val viewModel: SignUpViewModel by viewModels()
@@ -34,12 +29,5 @@ class SignUpFragment : Fragment()  {
             val password = binding.passwordInputEditTextRegister.text.toString()
             viewModel.onRegisterClicked(email, password)
         }
-        viewModel.isSuccessfullSignUp.observe(viewLifecycleOwner, Observer{ isSuccessful ->
-            if (isSuccessful) {
-                view.findNavController().navigate(R.id.action_signUpFragment_to_menuFragment)
-            } else {
-                toast("Sign up failed, password must be at least 6 characters")
-            }
-        })
     }
 }

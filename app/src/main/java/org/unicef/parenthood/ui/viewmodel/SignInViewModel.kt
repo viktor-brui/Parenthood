004dae81be350531955cb78cd17732cefe1fc800
kutovenko <kutovenko@gmail.com>
2019-11-23 05:17:52 +0300

    FakeRepository deleted

--- app/src/main/java/org/unicef/parenthood/ui/viewmodel/SignInViewModel.kt
+++ app/src/main/java/org/unicef/parenthood/ui/viewmodel/SignInViewModel.kt
@@ -19,21 +19,18 @@ class SignInViewModel: ViewModel() {
 
 
     fun onLoginClicked(email: String, password: String) {
-        val repo = Repository()
-        val list = repo.getRecommended()
-
-//        auth.signInWithEmailAndPassword(email, password)
-//            .addOnCompleteListener { task ->
-//                if (task.isSuccessful) {
-//                    // Sign in success, update UI with the signed-in user's information
-//                    Log.d(TAG, "signInWithEmail:success")
-//                    val user = auth.currentUser
-//                    _isSuccessfulLogin.value = Event(true)
-//                } else {
-//                    // If sign in fails, display a message to the user.
-//                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-//                    _isSuccessfulLogin.value = Event(false)
-//                }
-//            }
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    _isSuccessfulLogin.value = Event(true)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    _isSuccessfulLogin.value = Event(false)
+                }
+            }
     }
 }
